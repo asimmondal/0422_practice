bc8a8fb355c40dd6346980644d554a0dd18a71c9
Mondal Asim <mandaru.asim@gmail.com>
2015-04-22 05:39:49 +0000

    add sample03

---
+++ sample03.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+print('現在は' . date('G時　i分　s秒'). 'です');
+    ?>
+    
+</body>
+</html>
